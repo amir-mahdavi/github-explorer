11417e3a19962778f450739c58c869cf4c0ae2bd
Amir Mahdavi <amirmahdavi65@gmail.com>
2019-07-10 01:51:04 +1000

    Moved repos and getUserRepos to context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -44,6 +44,17 @@ const GithubState = props => {
   };
 
   // get repos
+  const getUserRepos = async username => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({ type: GET_REPOS, payload: res.data });
+  };
 
   // set loading
   const setLoading = () => dispatch({ type: SET_LOADING });
@@ -57,7 +68,8 @@ const GithubState = props => {
         loading: state.loading,
         searchUsers,
         clearUsers,
-        getUser
+        getUser,
+        getUserRepos
       }}
     >
       {props.children}
